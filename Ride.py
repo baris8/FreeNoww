f788bb51c687ac4910647e65619936819e95eeaf
Baris <uectas.ba@gmail.com>
2020-11-08 15:08:19 +0100

    Convert Price Decimal-Point to a comma, so Excel wont interpret the value as a date

--- Ride.py
+++ Ride.py
@@ -8,6 +8,6 @@ class Ride:
 
     def export_for_csv(self):
         if self.pm == "APP":
-            return self.date + ";" + self.lp + ";;" + str(self.price) + "\n"
+            return self.date + ";" + self.lp + ";;" + str(self.price).replace('.', ',') + "\n"
         else:
-            return self.date + ";" + self.lp + ";" + str(self.price) + ";\n"
+            return self.date + ";" + self.lp + ";" + str(self.price).replace('.', ',') + ";\n"
